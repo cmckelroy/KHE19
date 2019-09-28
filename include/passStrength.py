a8968405b17a9ef7093f741c7e4df1e1a87454b4
Alex Sweeney <x2110311x@gmail.com>
2019-09-28 04:36:54 -0400

    Added PasswordStrength

---
+++ include/passStrength.py
@@ -0,0 +1,30 @@
+"""
+passStrength.py
+------------
+
+Defines the classes and functions necessary to determine password strength
+"""
+
+from zxcvbn import zxcvbn
+
+
+class PasswordStrength:
+    """Represents the strengh of a password
+    
+    Attributes
+    ------------
+    __passvalue: :class:`str`
+        The actual value of the password
+    score: :class:`int`
+        A score from 0-4 (0 is lowest) of the password strength
+    warnings :class:`str`
+        Warnings about the password
+    suggestions: :class:`list`
+        A list of suggestions for the password
+    """
+    def __init__(self, password):
+        self.__passvalue = password
+        self.__strengthResult = zxcvbn(self.__passvalue)
+        self.score = int(self.__strengthResult['score'])
+        self.warnings = self.__strengthResult['feedback']['warning']
+        self.suggestions = self.__strengthResult['feedback']['suggestions']
